3786f4114d406197ed730065dcdfcc98fc80f635
sidecodes <sidekicktal@outlook.com>
2020-10-30 01:48:00 +0530

    Added API

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 export default function SearchMovies(){
+    
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+        
+        const query = "Jurassic Park";
+        
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=d2c88103b571caaeaa6a994c0dbedc33&language=en-US&query=${query}&page=1&include_adult=false`;
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        }catch(err){
+            console.error(err);
+        }
+    }
+    
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
                 placeholder="i.e. Jurassic Park"/>
