1d4842bb0c3fa8bb7d8bdd68a5f5fad631fee7e2
sidecodes <sidekicktal@outlook.com>
2020-10-30 02:36:03 +0530

    Added Poster Path

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,30 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function SearchMovies(){
     
+    //states- input query, movies
+    const [query, setQuery] = useState('');
+
+    //create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
-        
-        const query = "Jurassic Park";
         
         const url = `https://api.themoviedb.org/3/search/movie?api_key=d2c88103b571caaeaa6a994c0dbedc33&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
+            setMovies(data.results);
         }catch(err){
             console.error(err);
         }
     }
     
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park"/>
-            <button className="button" type="submit">Search</button>
-        </form>
+
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input className="input" type="text" name="query"
+                    placeholder="i.e. Jurassic Park"
+                    value={query} onChange={(e) => setQuery(e.target.value)}
+                    />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                    <div className="card" key={movie.id}>
+                        <img className="card--image"
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            alt={movie.title + ' poster'}
+                            />
+
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
