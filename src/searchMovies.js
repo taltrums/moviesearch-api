b753cb72b4aee2faf7ffc99c56ac0e309c9f015c
sidecodes <sidekicktal@outlook.com>
2020-10-31 01:10:58 +0530

    Updated search

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import MovieCard from './movieCard.js';
 
 export default function SearchMovies(){
     
@@ -35,13 +36,7 @@ export default function SearchMovies(){
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card--image"
-                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + ' poster'}
-                            />
-
-                    </div>
+                    <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
         </>
